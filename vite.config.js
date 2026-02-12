927f0dc44b9454158603ba2f1310e5a1f8a8be67
lbwry782-star <lbwry782@gmail.com>
2026-02-13 01:22:06 +0200

    תיקון

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/ACE-frontend/',
   plugins: [react()],
   server: {
     port: 3000,
