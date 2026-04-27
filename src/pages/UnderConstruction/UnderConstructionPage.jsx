1bb89a8fe2f00419d625807d867de13087c2d01a
lbwry782-star <lbwry782@gmail.com>
2026-04-27 10:18:16 +0300

    UI

--- src/pages/UnderConstruction/UnderConstructionPage.jsx
+++ src/pages/UnderConstruction/UnderConstructionPage.jsx
@@ -5,10 +5,9 @@ const openingVideoSrc = `${BASE_URL}assets/${encodeURIComponent('ווידאו_פ
 const welcomePng = `${BASE_URL}assets/${encodeURIComponent('ברוכים הבאים.png')}`
 const agreeSrc = `${BASE_URL}assets/${encodeURIComponent('אני מסכים.png')}`
 const checkboxSrc = `${BASE_URL}assets/${encodeURIComponent('תיבת סימון.png')}`
-const spacer2Src = `${BASE_URL}assets/${encodeURIComponent('רווח2.png')}`
+const spacePng = `${BASE_URL}assets/${encodeURIComponent('רווח.png')}`
 const termsSrc = `${BASE_URL}assets/${encodeURIComponent('לצפיה בתנאים.png')}`
 const space4Png = `${BASE_URL}assets/${encodeURIComponent('רווח4.png')}`
-const spacePng = `${BASE_URL}assets/${encodeURIComponent('רווח.png')}`
 const videoPng = `${BASE_URL}assets/${encodeURIComponent('וידאו.png')}`
 const space3Png = `${BASE_URL}assets/${encodeURIComponent('רווח3.png')}`
 const adPng = `${BASE_URL}assets/${encodeURIComponent('מודעה.png')}`
@@ -27,25 +26,22 @@ function UnderConstructionPage() {
 
       <div className="uc-content">
         <div className="uc-block">
-          <img src={welcomePng} alt="welcome" className="uc-welcome-png" />
+          <img src={welcomePng} alt="welcome" className="uc-img" />
 
-          <div className="uc-png-row" dir="rtl">
-            <img src={agreeSrc} alt="agree" />
-            <img src={checkboxSrc} alt="checkbox" />
-            <img src={spacer2Src} alt="space" />
-            <img src={termsSrc} alt="terms" />
+          <div className="uc-top-row">
+            <img src={agreeSrc} alt="agree" className="uc-img" />
+            <img src={checkboxSrc} alt="checkbox" className="uc-img" />
+            <img src={spacePng} alt="space" className="uc-img" />
+            <img src={termsSrc} alt="terms" className="uc-img" />
           </div>
 
-          <div className="uc-bottom-wrapper">
-            <div className="uc-space-anchor">
-              <img src={spacePng} alt="space" />
-              <img src={space4Png} alt="space4" />
-            </div>
+          <div className="uc-lower-row">
+            <img src={space4Png} alt="space4" className="uc-img" />
 
-            <div className="uc-bottom-row">
-              <img src={videoPng} alt="video" />
-              <img src={space3Png} alt="space3" />
-              <img src={adPng} alt="ad" />
+            <div className="uc-choice-row">
+              <img src={videoPng} alt="video" className="uc-img" />
+              <img src={space3Png} alt="space3" className="uc-img" />
+              <img src={adPng} alt="ad" className="uc-img" />
             </div>
           </div>
         </div>
