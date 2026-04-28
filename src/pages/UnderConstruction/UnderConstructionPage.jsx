5442e4b80f9325712de2e332691cdb4dd296a41a
lbwry782-star <lbwry782@gmail.com>
2026-04-28 05:02:57 +0300

    WELCOME

--- src/pages/UnderConstruction/UnderConstructionPage.jsx
+++ src/pages/UnderConstruction/UnderConstructionPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./UnderConstructionPage.css";
+import png11 from "../../assets/11.png";
 
 const bgVideo = `${import.meta.env.BASE_URL}assets/${encodeURIComponent("ווידאו_פתיחה.mp4")}`;
 
@@ -13,6 +14,11 @@ export default function UnderConstructionPage() {
         loop
         muted
         playsInline
+      />
+      <img
+        src={png11}
+        className="uc-center-image"
+        alt="center visual"
       />
     </div>
   );
