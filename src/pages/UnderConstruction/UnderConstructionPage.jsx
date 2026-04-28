52e75dae93d7820ea4b34be33fb85cc5252c6753
lbwry782-star <lbwry782@gmail.com>
2026-04-28 05:23:26 +0300

    UI

--- src/pages/UnderConstruction/UnderConstructionPage.jsx
+++ src/pages/UnderConstruction/UnderConstructionPage.jsx
@@ -14,11 +14,19 @@ export default function UnderConstructionPage() {
         muted
         playsInline
       />
-      <img
-        src="/assets/11.png?v=2"
-        className="uc-center-image"
-        alt="center visual"
-      />
+      <div className="uc-image-block">
+        <img
+          src="/assets/11.png?v=2"
+          className="uc-center-image"
+          alt="main"
+        />
+        <div className="uc-image-row">
+          <img src="/assets/22.png?v=2" alt="22" />
+          <img src="/assets/33.png?v=2" alt="33" />
+          <img src="/assets/44.png?v=2" alt="44" />
+          <img src="/assets/55.png?v=2" alt="55" />
+        </div>
+      </div>
     </div>
   );
 }
